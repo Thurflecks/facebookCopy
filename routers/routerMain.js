a2bb3d086f7cceff63e729b9de3ec5d3259a9d3b
Thurflecks <thurflecks.dev@gmail.com>
2024-09-18 17:14:46 -0300

    css

--- routers/routerMain.js
+++ routers/routerMain.js
@@ -2,6 +2,23 @@ const express = require("express")
 const router = express.Router()
 
 
+router.get("/login", (req, res) => {
+    res.render("login")
+})
+
+router.post("/login/conta", (req, res) => {
+    res.send("conta logada")
+})
+
+router.get("/criarConta", (req, res) => {
+    res.render("contaNova")
+})
+
+router.post("/criarConta/criando", (req, res) => {
+    res.redirect("/")
+})
+
+
 router.get("/", (req, res) =>{
     res.render("homepage")
 })
