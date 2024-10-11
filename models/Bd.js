2cd9a681155fe4a56e7959bd9dbd2ad077c469b8
Thurflecks <thurflecks.dev@gmail.com>
2024-10-11 13:47:46 -0300

    banco de dados para deploy

--- models/Bd.js
+++ models/Bd.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize("facebook", "root", "", {
-    host: "localhost",
+const sequelize = new Sequelize("sql10737143", "sql10737143", "aDwXIWx8jA", {
+    host: "sql10.freesqldatabase.com",
     dialect: "mysql"
 })
 
