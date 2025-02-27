737ede00a0334664ef19f1596e3af587e43811de
Thurflecks <thurflecks.dev@gmail.com>
2025-02-27 15:53:26 -0300

    vercel

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const router = require("./routers/routerMain.js")
 const { engine } = require("express-handlebars")
 const session = require("express-session")
 //porta
-const port = 8081
+const port = process.env.PORT | 8081
 
 //config pastas de arquivos estaticos
 app.use(express.static(path.join(__dirname, "public")))
